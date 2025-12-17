d46e9222b0fe3f4ecc1daa255c6955c6d961d53d
rubb3rDucc <mingos1337@gmail.com>
2025-12-16 21:30:28 -0500

    update the fronted pkg.json and vite.config, and added terser

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Output directory
+    minify: 'terser', // Minify files using Terser
+    sourcemap: false // Do not Generate source maps
+  },
 })
